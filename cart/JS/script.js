1fffecb8f710b0f5c06864b4bcf84b93d1bf6dd3
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-02-15 23:00:53 -0800

    Add files via upload

---
+++ cart/JS/script.js
@@ -0,0 +1,11 @@
+// Show Real Products
+
+let products = [
+  {
+    name: "Wireless-headphones",
+    heading: "headphones for your Style",
+    price: "5600",
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
