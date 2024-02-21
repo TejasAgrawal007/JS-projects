f34c5bd748f31dd2a7d58ba0219aa1ed9855d74e
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-02-21 15:04:04 +0530

    Update script.js

--- cart/JS/script.js
+++ cart/JS/script.js
@@ -1,115 +1,94 @@
-let products = [
+let product = [
   {
-    name: "Wireless-headphones",
-    heading: "headphones for your Style",
-    price: "5600",
+    name: "Black Product",
+    heading: "The Best Eye liner",
     image:
-      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1676717962720-d9a812c8f8c9?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "12,000",
   },
   {
-    name: "Lumin",
-    heading: "Charcole Clinser",
-    price: "3400",
+    name: "Serum",
+    heading: "The Best Your Body",
     image:
-      "https://images.unsplash.com/photo-1567721913486-6585f069b332?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1627384113743-6bd5a479fffd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "3000",
   },
   {
-    name: "Curology",
-    heading: "The Rich Moisurizer",
-    price: "3400",
+    name: "Black Product",
+    heading: "The Best Eye liner",
     image:
-      "https://images.unsplash.com/photo-1571781926291-c477ebfd024b?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-
-let popular = [
-  {
-    name: "White Chair",
-    heading: "A comfort",
-    image:
-      "https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "13,000",
+      "https://plus.unsplash.com/premium_photo-1676717962720-d9a812c8f8c9?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "12,000",
   },
   {
-    name: "Relaxing Chair",
-    heading: " Chair ",
+    name: "Serum",
+    heading: "The Best Your Body",
     image:
-      "https://images.unsplash.com/photo-1600093328589-39ed150b382b?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "24,000",
+      "https://images.unsplash.com/photo-1627384113743-6bd5a479fffd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "3000",
   },
 ];
 
 let cart = [];
 
-// Show Popular Products
-function showProducts() {
+function showCards() {
   let clutter = "";
-  products.forEach(function (e, index) {
-    clutter += `   <div class="product w-fit rounded-xl p-2 bg-white">
-                      <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl overflow-hidden">
-                      <img class="h-full w-full object-cover" src="${e.image}" alt="">
-                      </div>
-                      <div class="data w-full px-2 py-5">
-                        <h1 class="font-semibold text-xl leading-none tracking-tight">
-                        ${e.name}
-                        </h1>
-                        <div class="flex justify-between w-full items-center mt-2">
-                          <div class="w-1/2">
-                            <h3 class="font-semibold opacity-20">${e.heading}</h3>
-                            <h4 class="font-semibold mt-2">₹ ${e.price}</h4>
-                          </div>
-                          <button data-index="${index}"
-                            class="add w-10 h-10 rounded-full bg-[#4A4563] text-yellow-400"
-                          >
-                            <i data-index="${index} class="add ri-add-line"></i>
-                          </button>
-                        </div>
-                      </div>
-                    </div>`;
+  product.forEach(function (e, index) {
+    clutter += ` <div class="product w-fit rounded-xl p-2 bg-white">
+        <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl">
+            <img class="h-full w-full object-cover" src="${e.image}" alt="">
+        </div>
+        <div class="data w-full px-2 py-5">
+          <h1 class="font-semibold text-xl leading-none tracking-tight">
+            ${e.name}
+          </h1>
+          <div class="flex justify-between w-full items-center mt-2">
+            <div class="w-1/2">
+              <h3 class="font-semibold opacity-20">${e.price}</h4>
+            </div>
+            <button data-index="${index}" class="add w-10 h-10 rounded-full shader text-yellow-400">
+              <i data-index="${index}" class="add ri-add-line"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+     `;
   });
 
   document.querySelector(".products").innerHTML = clutter;
 }
 
-let clutter = "";
-function showPopularProducts() {
-  popular.forEach(function (e) {
-    clutter += `<div
-    class="popular bg-white p-2 rounded-2xl flex items-start gap-3 w-[60%] flex-shrink-0">
-    <div
-      class="w-20 h-20 bg-red-500 flex-shrink-0 rounded-2xl border-4 border-white overflow-hidden"
-    >
-      <img
-        class="w-full h-full object-cover"
-        src="${e.image}"
-        alt=""
-      />
-    </div>
-    <div class="data py-2 w-full">
-      <h1 class="leading-none font-semibold">${e.name}</h1>
-      <h4 class="leading-none mt-2 text-sm font-semibold opacity-20">
-        ${e.heading}
-      </h4>
-      <h4 class="mt-3 font-semibold text-zinc-500">$${e.price}</h4>
-    </div>
-  </div>`;
-  });
-
-  document.querySelector(".populars").innerHTML = clutter;
-}
-
-// Add To Cart
 function addToCart() {
   document
     .querySelector(".products")
     .addEventListener("click", function (details) {
       if (details.target.classList.contains("add")) {
-        cart.push(products[details.target.dataset.index]);
+        cart.push(product[details.target.dataset.index]);
         console.log(cart);
       }
     });
 }
 
+function showCart() {
+  document.querySelector(".carticon").addEventListener("click", function () {
+    document.querySelector(".cartexpnd").style.display = "block";
+
+    let clutter = "";
+    cart.forEach(function (product, index) {
+      clutter += `<div class="flex gap-2 bg-white p-2 rounded-lg">
+            <div class="w-10 h-10 flex-shrink-0 rounded-lg bg-red-500">
+                <img class="h-full w-full object-cover" src="${product.image}" alt="">
+            </div>
+            <div>
+                <h3 class="text-semibold">${product.name}</h3>
+                <h5 class="text-sm font-semibold opacity-80">${product.price}</h5>
+            </div>
+        </div>`;
+    });
+    document.querySelector(".cartexpnd").innerHTML = clutter;
+  });
+}
+
+showCart();
 addToCart();
-showProducts();
-showPopularProducts();
+showCards();
